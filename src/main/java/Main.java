163d1e3e25ab7a741879c152eb31a4ec85d1fe6c
Michał Suliborski <msuliborski@gmail.com>
2019-03-11 13:40:20 +0100

    duqwaf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        SudokuBoard sudoku = new SudokuBoard();
+        sudoku.prefill();
+        sudoku.display();
     }
 }
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,7 +1,38 @@
 public class SudokuBoard {
-    private int[][] board = new int[9][9];
+    private int[][] board;
 
-    public void fillBoard(){
-        
+    public void prefill(){
+        //puts values to make actual sudoku
+        board = new int[][] {   {0, 0, 0,   0, 0, 0,    0, 8, 0},
+                                {0, 0, 0,   3, 8, 6,    5, 0, 2},
+                                {0, 0, 0,   0, 0, 1,    3, 0, 0},
+
+                                {0, 4, 0,   0, 9, 5,    2, 0, 3},
+                                {0, 0, 0,   4, 0, 0,    0, 0, 5},
+                                {3, 1, 0,   0, 0, 2,    6, 4, 0},
+
+                                {0, 2, 1,   0, 0, 0,    4, 0, 6},
+                                {0, 8, 0,   0, 0, 0,    0, 0, 0},
+                                {0, 0, 3,   0, 0, 0,    1, 0, 0}};
+    }
+
+    public void fillSudoku(){
+        //method to solve sudoku
+    }
+
+    public void validate(){
+
+    }
+
+    public void display(){
+        for (int x = 0; x < 9; x++){
+            for (int y = 0; y < 9; y++){
+                System.out.print(board[x][y] + " ");
+                if (y % 3 == 2 && y != 8 ) System.out.print("| ");
+            }
+            System.out.println();
+            if (x % 3 == 2 && x != 8 ) System.out.println("- - - - - - - - - - -");
+        }
+
     }
 }
