6480575a23cef0a7b20bac16dfd48f8518cc16de
jedrekszor <jedrekszor@wp.pl>
2019-03-11 13:40:13 +0100

    kdjfhshj

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,7 +1,15 @@
-public class SudokuBoard {
-    private int[][] board = new int[9][9];
+import static java.lang.StrictMath.sqrt;
 
-    public void fillBoard(){
-        
+public class SudokuBoard {
+    private static int[][] board = new int[9][9];
+
+    public static void main(String args[]) {
+    //public void fillBoard(int arr[][], int row, int column) {
+        for(int i=0; i<sqrt(board.length); i++) {
+            for(int j=0; j<sqrt(board.length); j++) {
+                i = 7;
+                i = i/3;
+            }
+        }
     }
 }
