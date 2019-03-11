8122fd8706ff4fa9cef8ffa6e18129f35348281d
Michał Suliborski <msuliborski@gmail.com>
2019-03-11 13:46:16 +0100

    wadwaf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,25 @@ import static java.lang.StrictMath.sqrt;
 
 public class Main {
     public static void main(String[] args) {
+
         SudokuBoard sudoku = new SudokuBoard();
+
         sudoku.prefill();
+
+        System.out.println();
+        System.out.println("Sudoku to solve:");
+        System.out.println();
+        sudoku.display();
+
+        System.out.println();
+        System.out.println("Now we are solving sudoku...");
+        System.out.println();
+
+        sudoku.fillSudoku();
+
+        System.out.println();
+        System.out.println("Here is solved sudoku:");
+        System.out.println();
         sudoku.display();
 
         //public void fillBoard(int arr[][], int row, int column) {
@@ -11,6 +28,7 @@ public class Main {
             for(int j=0; j<sqrt(sudoku.board.length); j++) {
                 i = 7;
                 i = i/3;
+                System.out.println(i);
             }
         }
     }
