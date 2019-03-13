fe83949ca27695335537eb7a4c1942b68eff3622
Michał Suliborski <msuliborski@gmail.com>
2019-03-13 10:59:04 +0100

    wad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,23 +14,13 @@ public class Main {
         System.out.println("Now we are solving sudoku...");
         System.out.println();
 
-        sudoku.fillSudoku();
+        if (sudoku.fillSudoku()){
 
-        System.out.println();
-        System.out.println("Here is solved sudoku:");
-        System.out.println();
-        sudoku.display();
+            System.out.println();
+            System.out.println("Here is solved sudoku:");
+            System.out.println();
+            sudoku.display();
+        }
 
-        //public void fillBoard(int arr[][], int row, int column) {
-
-//        for(int i=0; i<sudoku.board.length; i++) {
-//            for(int j=0; j<sudoku.board.length; j++) {
-//                for(int num=1; num<=9; num++) {
-//                    sudoku.check(sudoku.board, num, i, j);
-//                }
-//            }
-//        }
-        if (!sudoku.check(sudoku.board, 6, 4, 0)) System.out.print("nie da rady cycu :/");
-        else System.out.print("da rade na czilku mordo :D");
     }
 }
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -3,50 +3,87 @@ import static java.lang.StrictMath.sqrt;
 public class SudokuBoard {
     public int[][] board;
 
-    public void prefill(){
+    public void prefill() {
         //puts values to make actual sudoku
-        board = new int[][] {   {0, 0, 0,   0, 0, 0,    0, 8, 0},
-                                {0, 0, 0,   3, 8, 6,    5, 0, 2},
-                                {0, 0, 0,   0, 0, 1,    3, 0, 0},
+        board = new int[][]{
+                {2, 9, 0, 0, 0, 0, 0, 8, 0},
+                {0, 0, 0, 3, 8, 6, 5, 0, 2},
+                {0, 0, 0, 0, 0, 1, 3, 0, 0},
 
-                                {0, 4, 0,   0, 9, 5,    2, 0, 3},
-                                {0, 0, 0,   4, 0, 0,    0, 0, 5},
-                                {3, 1, 0,   0, 0, 2,    6, 4, 0},
+                {0, 4, 0, 0, 9, 5, 2, 0, 3},
+                {0, 0, 0, 4, 0, 0, 0, 0, 5},
+                {3, 1, 0, 0, 0, 2, 6, 4, 0},
 
-                                {0, 2, 1,   0, 0, 0,    4, 0, 6},
-                                {0, 8, 0,   0, 0, 0,    0, 0, 0},
-                                {0, 0, 3,   0, 0, 0,    1, 0, 0}};
+                {0, 2, 1, 0, 0, 0, 4, 0, 6},
+                {0, 8, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 3, 0, 0, 0, 1, 0, 0}};
     }
 
-    public void fillSudoku(){
-        //method to solve sudoku
+    public boolean areThereZeros() {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                if (board[x][y] == 0) return true;
+            }
+        }
+        return false;
     }
 
-    public void validate(){
+    public boolean fillSudoku() {
+        if (!areThereZeros()) return true;
 
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+
+
+
+                for (int i = 1; i < 10; i++) {
+                    if (check(i, x, y)) {
+                        board[x][y] = i;
+                        return fillSudoku();
+                    }
+                }
+
+                board[x][y] = 0;
+
+                if (y == 0) board[x-1][8]++;
+                else board[x][y-1]++;
+
+                return fillSudoku();
+
+
+
+
+
+            }
+        }
+
+        return false;
     }
 
-    public void display(){
-        for (int x = 0; x < 9; x++){
-            for (int y = 0; y < 9; y++){
+
+    public void display() {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
                 System.out.print(board[x][y] + " ");
-                if (y % 3 == 2 && y != 8 ) System.out.print("| ");
+                if (y % 3 == 2 && y != 8) System.out.print("| ");
             }
             System.out.println();
-            if (x % 3 == 2 && x != 8 ) System.out.println("- - - - - - - - - - -");
+            if (x % 3 == 2 && x != 8) System.out.println("- - - - - - - - - - -");
         }
     }
 
-    public boolean check(int arr[][], int num, int row, int column) {
-        for(int i=0; i<arr.length; i++) {
-            if((arr[row][i] == num && i != column) || (arr[i][column] == num && i != row)) return false;
+    public boolean check(int num, int row, int column) {
+        if (num == 0) return false;
+        for (int i = 0; i < board.length; i++) {
+            if ((board[row][i] == num && i != column) || (board[i][column] == num && i != row)) return false;
         }
-        int newRow = (row/3)*3;
-        int newColumn = (column/3)*3;
+        int newRow = (row / 3) * 3;
+        int newColumn = (column / 3) * 3;
 
-        for(int i=0; i<3; i++) {
-            for(int j=0; j<3; j++) {
-                if((arr[newRow + i][newColumn + j] == num) && newRow+i != row && newColumn+j != column) return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((board[newRow + i][newColumn + j] == num) && newRow + i != row && newColumn + j != column)
+                    return false;
             }
         }
         return true;
