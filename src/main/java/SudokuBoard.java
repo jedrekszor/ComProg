85ea627686640ecc5686cb4bf4c6ec4deedff855
Michał Suliborski <msuliborski@gmail.com>
2019-03-13 14:02:51 +0100

    wad

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,22 +1,19 @@
-import static java.lang.StrictMath.sqrt;
-
 public class SudokuBoard {
     public int[][] board;
 
     public void prefill() {
-        //puts values to make actual sudoku
         board = new int[][]{
-                {2, 9, 0, 0, 0, 0, 0, 8, 0},
-                {0, 0, 0, 3, 8, 6, 5, 0, 2},
-                {0, 0, 0, 0, 0, 1, 3, 0, 0},
+                {7, 3, 0, 2, 0, 1, 0, 0, 0},
+                {6, 0, 0, 0, 7, 3, 1, 2, 5},
+                {0, 2, 1, 0, 0, 4, 0, 6, 0},
 
-                {0, 4, 0, 0, 9, 5, 2, 0, 3},
-                {0, 0, 0, 4, 0, 0, 0, 0, 5},
-                {3, 1, 0, 0, 0, 2, 6, 4, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 1},
+                {0, 0, 6, 0, 0, 0, 2, 0, 0},
+                {3, 1, 0, 0, 0, 0, 0, 0, 4},
 
-                {0, 2, 1, 0, 0, 0, 4, 0, 6},
-                {0, 8, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 3, 0, 0, 0, 1, 0, 0}};
+                {0, 5, 0, 1, 0, 0, 3, 8, 0},
+                {4, 8, 3, 7, 5, 0, 0, 0, 6},
+                {0, 0, 0, 8, 0, 9, 0, 5, 2}};
     }
 
     public boolean areThereZeros() {
@@ -29,39 +26,35 @@ public class SudokuBoard {
     }
 
     public boolean fillSudoku() {
+
         if (!areThereZeros()) return true;
 
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
 
 
+                if (board[x][y] == 0) {
 
-                for (int i = 1; i < 10; i++) {
-                    if (check(i, x, y)) {
-                        board[x][y] = i;
-                        return fillSudoku();
+                    for (int i = 1; i < 10; i++) {
+                        if (check(i, x, y)) {
+                            board[x][y] = i;
+                            if (!fillSudoku()) board[x][y] = 0;
+                            else return true;
+                        }
                     }
+                    board[x][y] = 0;
+                    return false;
                 }
-
-                board[x][y] = 0;
-
-                if (y == 0) board[x-1][8]++;
-                else board[x][y-1]++;
-
-                return fillSudoku();
-
-
-
-
 
             }
         }
-
         return false;
     }
 
 
     public void display() {
+        System.out.println();
+        System.out.println("---------------------------------");
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
                 System.out.print(board[x][y] + " ");
@@ -70,10 +63,11 @@ public class SudokuBoard {
             System.out.println();
             if (x % 3 == 2 && x != 8) System.out.println("- - - - - - - - - - -");
         }
+        System.out.println("---------------------------------");
+        System.out.println();
     }
 
     public boolean check(int num, int row, int column) {
-        if (num == 0) return false;
         for (int i = 0; i < board.length; i++) {
             if ((board[row][i] == num && i != column) || (board[i][column] == num && i != row)) return false;
         }
---
+++ src/test/java/ex1/SudokuBoardTest.java
@@ -0,0 +1,20 @@
+package ex1;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SudokuBoardTest {
+
+
+
+    SudokuBoardTest() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGreetMethodWithNullValue() {
+
+    }
+
+}
