33e6646280ba9472d7eed56976c5105afe917453
jedrek <jedrekszor@wp.pl>
2019-03-17 21:03:06 +0100

    dhsfghfd

--- src/main/java/ex1/SudokuBoard.java
+++ src/main/java/ex1/SudokuBoard.java
@@ -16,6 +16,10 @@ public class SudokuBoard {
         }
     }
 
+    public int[][] getBoard()
+    {
+        return board;
+    }
 
     void fillSudoku() {
         this.randomSolveSudoku();
--- src/test/java/ex1/SudokuBoardTest.java
+++ src/test/java/ex1/SudokuBoardTest.java
@@ -15,7 +15,32 @@ public class SudokuBoardTest {
 
     @org.junit.jupiter.api.Test
     void testIfFillBoardGeneratesCorrectDigitsLayout() {
+        SudokuBoard sudoku = new SudokuBoard();
 
+        sudoku.fillSudoku();
+
+        boolean test = false;
+
+        for(int x = 0; x < sudoku.getBoard().length; x++)
+        {
+            for(int y = 0; y < sudoku.getBoard().length; y++)
+            {
+                for (int i = 0; i < sudoku.getBoard().length; i++) {
+                    if ((sudoku.getBoard()[x][i] == sudoku.getBoard()[x][y] && i != y) || (sudoku.getBoard()[i][y] == sudoku.getBoard()[x][y] && i != x)) test = false;
+                }
+                int newRow = (x / 3) * 3;
+                int newColumn = (y / 3) * 3;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if ((sudoku.getBoard()[newRow + i][newColumn + j] == sudoku.getBoard()[x][y]) && newRow + i != x && newColumn + j != y)
+                            test =  false;
+                    }
+                }
+                test =  true;
+            }
+        }
+        assertTrue(test);
     }
 
     @org.junit.jupiter.api.Test
