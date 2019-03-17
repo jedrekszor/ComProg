f6ff8b056bb69a4a3533f625105c35e7f426d1d8
Michał Suliborski <msuliborski@gmail.com>
2019-03-17 20:58:56 +0100

    wad

--- src/test/java/ex1/SudokuBoardTest.java
+++ src/test/java/ex1/SudokuBoardTest.java
@@ -21,6 +21,22 @@ public class SudokuBoardTest {
     @org.junit.jupiter.api.Test
     void testIfTwoSubsequentCallOfFillBoardGeneratesDifferentDigitsLayout() {
         SudokuBoard sudoku = new SudokuBoard();
+        sudoku.fillSudoku();
+        int[][] board1 = sudoku.getBoard();
+        sudoku.fillSudoku();
+        int[][] board2 = sudoku.getBoard();
+
+        StringBuilder boardstring1 = new StringBuilder();
+        StringBuilder boardstring2 = new StringBuilder();
+
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                boardstring1.append(board1[x][y]);
+                boardstring2.append(board2[x][y]);
+            }
+        }
+        assertEquals(boardstring1.toString(), boardstring2.toString());
+
     }
 
 
